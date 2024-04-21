cce96a6fa52696d407d1f948c44bc71c8c159969
Kristel Kolkanen <kristel.kolkanen@gmail.com>
2024-04-21 23:58:09 +0300

    upload image feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,11 @@ export class AppComponent {
     { title: 'My Journal', url: '/folder/Journal', icon: 'book' },
     { title: 'Travel Map', url: '/folder/Travel Map', icon: 'map' },
     { title: 'Bucket List', url: '/folder/Bucket List', icon: 'list' },
-    { title: 'My Travel Statistics', url: '/folder/Travel Stats', icon: 'stats-chart' },
+    {
+      title: 'My Travel Statistics',
+      url: '/folder/Travel Stats',
+      icon: 'stats-chart',
+    },
     { title: 'Gallery', url: '/folder/Gallery', icon: 'camera' },
     { title: 'Records', url: '/folder/Records', icon: 'mic' },
   ];
--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
+defineCustomElements(window);
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic()
-	.bootstrapModule(AppModule)
-	.catch((err) => console.log(err));
+  .bootstrapModule(AppModule)
+  .catch((err) => console.log(err));
 
 defineCustomElements(window);
