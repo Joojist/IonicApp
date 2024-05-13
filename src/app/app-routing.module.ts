92e5d87efdf59535f5634b19da4603fcbb6aeeae
Kristel Kolkanen <kristel.kolkanen@gmail.com>
2024-05-13 15:43:29 +0300

    refactor gallery feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
+import { GalleryComponent } from './gallery/gallery.component';
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { MapsComponent } from './map/maps.component';
-import {StatisticsComponent} from "./statistics/statistics.component";
+import { StatisticsComponent } from './statistics/statistics.component';
 import { BucketListComponent } from './bucket-list/bucket-list.component';
 
 const routes: Routes = [
@@ -18,12 +19,19 @@ const routes: Routes = [
   { path: 'travel-map', component: MapsComponent },
   {
     path: 'preferences',
-    loadChildren: () => import('./preferences/preferences.module').then( m => m.PreferencesPageModule)
+    loadChildren: () =>
+      import('./preferences/preferences.module').then(
+        (m) => m.PreferencesPageModule,
+      ),
   },
   { path: 'stats', component: StatisticsComponent },
   {
     path: 'bucket-list',
     component: BucketListComponent,
+  },
+  {
+    path: 'gallery',
+    component: GalleryComponent,
   },
 ];
 
